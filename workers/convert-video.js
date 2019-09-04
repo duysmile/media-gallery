cfebdeab01e037f7aec8a6f3f95d37f60d68cd9f
DuyN <duy210697@gmail.com>
2019-09-04 15:59:00 +0700

    update error handler kue

--- workers/convert-video.js
+++ workers/convert-video.js
@@ -6,27 +6,60 @@ const { videoRepository } = require('../repositories');
 class ConvertVideoWorker {
     process(job, done) {
         const { path, title, videoId } = job.data;
-        hbjs.spawn({
+        const convertVideoStream = hbjs.spawn({
             input: path,
             output: `uploads/${title}_${videoId}_480p.mp4`,
             // preset: 'Very Fast 720p30',
             preset: 'Very Fast 480p30',
-        })
-            .on('error', err => {
-                // invalid user input, no video found etc
-                console.error(err);
-            })
-            .on('progress', progress => {
-                console.log(
-                    'Percent complete: %s, ETA: %s',
-                    progress.percentComplete,
-                    progress.eta
-                )
-            })
-            .on('end', () => {
-                console.log('conversion success');
-                done();
+        });
+        convertVideoStream.on('error', err => {
+            // invalid user input, no video found etc
+            convertVideoStream.removeAllListeners();
+            return done(err);
+        }).on('progress', progress => {
+            console.log(
+                'Percent complete: %s, ETA: %s',
+                progress.percentComplete,
+                progress.eta
+            );
+        }).on('end', () => {
+            const fullPath = `uploads/${title}_${videoId}_480p.mp4`;
+            fs.stat(fullPath, (err, stats) => {
+                if (err) {
+                    return done(err);
+                }
+                const screenShot = `uploads/${title}_${videoId}_480p.jpeg`;
+                const screenShotVideo = new ffmpeg(fullPath);
+                const screeShotVideoStream = screenShotVideo.screenshots({
+                    count: 1,
+                    filename: `${title}_${videoId}_480p.jpeg`,
+                    folder: 'uploads',
+                    timemarks: ['30'], // number of seconds
+                    size: '320x240'
+                })
+
+                screeShotVideoStream.on('error', (err) => {
+                    screeShotVideoStream.removeAllListeners();
+                    return done(err);
+                }).on('end', () => {
+                    videoRepository.create({
+                        videoId,
+                        title,
+                        screenShot,
+                        type: {
+                            size: stats.size,
+                            url: fullPath,
+                            quality: '480p',
+                        }
+                    }).then(() => {
+                        console.log('stored DB successfully');
+                        done();
+                    }).catch(err => {
+                        done(err);
+                    });
+                });
             });
+        });
     }
 
     create(queue, { videoId, title, path }) {
@@ -39,37 +72,7 @@ class ConvertVideoWorker {
             })
             .removeOnComplete(true)
             .on('complete', () => {
-                console.log(`Convert video ${path} completed.`);
-                // TODO: handle error
-                const fullPath = `uploads/${title}_${videoId}_480p.mp4`;
-                fs.stat(fullPath, (err, stats) => {
-                    if (err) {
-                        console.error(err);
-                        return;
-                    }
-                    const screenShot = `uploads/${title}_${videoId}_480p.jpeg`;
-                    const screenShotVideo = new ffmpeg(fullPath);
-                    screenShotVideo.screenshots({
-                        count: 1,
-                        filename: `${title}_${videoId}_480p.jpeg`,
-                        folder: 'uploads',
-                        timemarks: ['30'], // number of seconds
-                        size: '320x240'
-                    }).on('end', () => {
-                        videoRepository.create({
-                            videoId,
-                            title,
-                            screenShot,
-                            type: {
-                                size: stats.size,
-                                url: fullPath,
-                                quality: '480p',
-                            }
-                        }).then(() => {
-                            console.log('stored DB successfully');
-                        });
-                    });
-                });
+                console.log(`Conversion of file ${path} completed!`);
             })
             .on('failed attempt', (err, doneAttempt) => {
                 console.error(`Convert video ${path} failed with error ${err.message}`);
