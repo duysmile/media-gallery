4c9e47162ac9b8bc6a8ff23458c3c17af4d0dbcb
DuyN <duy210697@gmail.com>
2019-09-10 10:28:47 +0700

    update get video by videoId

--- apis/video.js
+++ apis/video.js
@@ -3,5 +3,6 @@ const videoController = require('../controllers/video');
 exports.load = (app) => {
     app.post('/api/v1/videos', videoController.uploadVideo);
     app.get('/api/v1/videos', videoController.showAllVideos);
-    app.get('/api/v1/videos/:videoId', videoController.loadVideo);
+    app.get('/api/v1/videos/load/:videoId', videoController.loadVideo);
+    app.get('/api/v1/videos/:videoId', videoController.getVideo);
 };
